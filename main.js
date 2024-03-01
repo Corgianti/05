1e64ef421a3d4ddedf62437ec670c8703a184a49
Sabina Fratczak <sabifox19@gmail.com>
2024-03-01 08:43:31 +0100

    wsystko dodano

---
+++ main.js
@@ -0,0 +1,10 @@
+input1 = document.getElementById("input1");
+table1 = document.querySelectorAll("table td");
+
+function funkcja1(){
+    table1[0].style.backgroundColor = `hsl(${input1.value},100%, 50%)`;
+    table1[1].style.backgroundColor = `hsl(${input1.value},80%, 50%)`;
+    table1[2].style.backgroundColor = `hsl(${input1.value},60%, 50%)`;
+    table1[3].style.backgroundColor = `hsl(${input1.value},40%, 50%)`;
+    table1[4].style.backgroundColor = `hsl(${input1.value},20%, 50%)`;
+}
